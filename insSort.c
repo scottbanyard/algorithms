93153a11e098c081e6f5815487ed52986976b62f
Scott Banyard <scottbanyard1@yahoo.co.uk>
2015-09-29 22:42:52 +0100

    fixed 0 error, should be working

--- insSort.c
+++ insSort.c
@@ -1,21 +1,20 @@
-// insertion sort algorithm : pg 18 in intro to algorithms book
+// insertion sort algorithm - pg 18 in intro to algorithms book
 // scott banyard 29/09/15
-
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 
-void sort() {
+int sort() {
 
-	int c, d, temp, n;
-	int array[500];
+	int c, d, t, n;
+	int array[1000];
 
 	printf("Enter length of array: \n");
 	scanf("%d", &n);
 
-	// enter array
+	// enter array using scan loop
 	printf("Enter %d integers\n", n);
 
 	for (c = 0; c < n; c++) {
@@ -26,17 +25,16 @@ void sort() {
 	for (c = 1; c <= n; c++) {
 		d = c;
 
-	// swapping method with 2 cards (to the left) then repeat if need to swap again
 		while ((d > 0) && (array[d] < array[d-1])) {
-			temp 		= array[d];
+			t 			= array[d];
 			array[d]	= array[d-1];
-			array[d-1] 	= temp;
+			array[d-1] 	= t;
 
 			d--;
 		}
 	}
 
-	// print array loop
+	// prints modified array using loop
 	printf("Array in the ascending order is: \n");
     
     for(c = 0; c <= n - 1; c++) {
@@ -51,4 +49,5 @@ int main() {
 
 	sort();
 	return 0;
+
 }

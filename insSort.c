7bb9af5056c860b995f8d3f79337488b8756cf15
Scott Banyard <scottbanyard1@yahoo.co.uk>
2015-09-30 14:21:26 +0100

    added comments again - final version

--- insSort.c
+++ insSort.c
@@ -1,3 +1,6 @@
+// insertion sort algorithms - pg 18 in intro to algorithms book
+// scott banyard 28/09/15
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,7 +8,7 @@
 
 void sort() {
 
-	int c, d, t, n;
+	int c, d, temp, n;
 	int array[1000];
 
 	printf("Enter length of array: \n");
@@ -13,18 +16,21 @@ void sort() {
 
 	printf("Enter %d integers\n", n);
 
+	// loop to enter array up to n-1 (arrays start at 0)
 	for (c = 0; c < n; c++) {
 		scanf("%d", &array[c]);
 	}
 
-	// sorting method; like a hand of cards [c] [c] [c] [c] [c]
+	// sorting method: like a hand of cards [a] [b] [c] [d] [e]
+	// while loop swaps cards to the left if [c] < [b] for e.g.
+	// so [a] [c] [b] [d] [e]
 	for (c = 1; c <= n; c++) {
 		d = c;
 
 		while ((d > 0) && (array[d] < array[d-1])) {
-			t 			= array[d];
+			temp 		= array[d];
 			array[d]	= array[d-1];
-			array[d-1] 	= t;
+			array[d-1] 	= temp;
 
 			d--;
 		}
@@ -32,6 +38,7 @@ void sort() {
 
 	printf("Array in the ascending order is: \n");
     
+    // loop to print array
     for(c = 0; c <= n - 1; c++) {
         printf("%d ", array[c]);
     }
